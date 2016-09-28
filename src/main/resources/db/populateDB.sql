4208fbb41d9c0a9b9544fb316303e3e140d17606
Yakov Burtsev <yakovburtsev@gmail.com>
2016-09-28 16:34:33 +0300

    Add init and populate scripts for table meals

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -11,3 +12,16 @@ VALUES ('Admin', 'admin@gmail.com', 'admin');
 INSERT INTO user_roles (role, user_id) VALUES
   ('ROLE_USER', 100000),
   ('ROLE_ADMIN', 100001);
+
+INSERT INTO meals (user_id, date_time, description, calories) VALUES
+  (100000, '2016-09-27 10:00:00', 'user breakfast', 500),
+  (100000, '2016-09-27 13:00:00', 'user launch', 1000),
+  (100000, '2016-09-27 18:00:00', 'user dinner', 600),
+  (100000, '2016-09-28 10:00:00', 'user breakfast', 300),
+  (100000, '2016-09-28 13:00:00', 'user launch', 700),
+  (100000, '2016-09-28 18:00:00', 'user dinner', 500),
+  (100001, '2016-09-27 10:00:00', 'admin breakfast', 500),
+  (100001, '2016-09-27 13:00:00', 'admin launch', 1200),
+  (100001, '2016-09-27 18:00:00', 'admin dinner', 500),
+  (100001, '2016-09-28 11:00:00', 'admin launch', 800),
+  (100001, '2016-09-28 17:00:00', 'admin dinner', 800);
